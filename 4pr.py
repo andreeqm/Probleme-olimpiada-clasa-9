746ea56b3e53e49fb9c40c969e89431379ba26e5
moscvinandreea <70642112+moscvinandreea@users.noreply.github.com>
2020-12-08 19:29:04 +0300

    Add files via upload

---
+++ 4pr.py
@@ -0,0 +1,13 @@
+#A-aria patratului si a dreptunghiuui
+#pp-lungimea gardului patrat
+#ld-latimea dreptunghiului
+#lp-latura patratului
+#lm-laura dreptunghiului
+#pd-lungimea gardului dreptunghic
+pp=48
+lp=48/4
+A=lp**2
+ld=9
+lm=A/9
+pd=2*(9+16)
+print("Lungimea gardului dreptunghic este de: ",pd,"metrii")
